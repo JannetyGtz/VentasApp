55b418a18f8d6c0d94f4088c3fa7685e13796778
JannetyGtz <jannetygtz@live.com>
2015-09-15 11:58:24 -0500

    Add column status for Users and Stores RIGHT

--- db/migrate/20150907183740_create_users.rb
+++ db/migrate/20150907183740_create_users.rb
@@ -6,6 +6,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :password_digest
       t.string :cellphone
       t.string :role
+      t.string :status
       t.timestamps null: false
       t.references :store
     end
